9323b52f201d98b5a6f729b3a0d8bf1c8ee1175e
Jared Moats <JaredMoats@outlook.com>
2018-12-05 17:18:11 -0500

    Checkpoint 5 -Components-Library: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-  <section className="album">
-
-  </section>
-);
+class Album extends Component {
+  render() {
+    return(
+      //the url will match /albums/slug. Provided by React Router. 
+      <section className="album">
+        <p>{ this.props.match.params.slug } Album will go here</p>
+      </section>
+    );
+  }
+}
 
 export default Album;
--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import albumData from './../data/albums';
 
-const Library = () => (
-  <section className="library">
-    Library will go here
-  </section>
-);
+class Library extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { albums: albumData };
+  }
+
+  render() {
+    return(
+      //call .map() on this.state.albums and return a <div> element that
+      //prints the title property of each album.
+      <section className="library">
+        {
+          //for Jacob: please explain the parameters in this map function
+          //and how the function is working
+          this.state.albums.map( (album, index) =>
+            //Links to the album/album.slug. The slug is defined in albums.js
+            <Link to={ `/album/${album.slug}` } key={ index }>
+              <img  src={ album.albumCover } alt={ album.title } />
+              <div>{ album.title }</div>
+              <div>{ album.artist }</div>
+              <div>{ album.songs.length } songs</div>
+            </Link>
+         )
+        }
+      </section>
+    );
+  }
+}
 
 export default Library;
