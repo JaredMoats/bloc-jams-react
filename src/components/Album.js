8d39a9443affbce984151aa778a3a573312ee4c0
Jared Moats <JaredMoats@outlook.com>
2018-12-05 17:48:41 -0500

    checkpoint 6 components album help

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+
+    //retrieve the album.slug object that matches
+    //this.props.match.params.slug
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+    });
+
+    this.state = {
+      album: album
+    };
+
+  }
+
   render() {
     return(
-      //the url will match /albums/slug. Provided by React Router. 
+      //the url will match /albums/slug. Provided by React Router.
       <section className="album">
-        <p>{ this.props.match.params.slug } Album will go here</p>
+      <section id="album-info">
+        <img
+        id="album-cover-art"
+        src={ this.state.album.albumCover }
+        alt={ this.state.album.title }
+        />
+        <div className="album-details">
+          <h1 id="album-title">{ this.state.album.title }</h1>
+          <h2 className="artist">{ this.state.album.artist }</h2>
+          <div id="release-info">{ this.state.album.releaseInfo }</div>
+        </div>
+      </section>
       </section>
     );
   }
